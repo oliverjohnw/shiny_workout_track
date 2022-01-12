906cccfc373cc40be7fe2b4de8a87bbec061c7ae
John Oliver <johnoliver@Johns-Air.attlocal.net>
2022-01-11 23:09:28 -0600

    update app

--- app.R
+++ app.R
@@ -9,7 +9,10 @@ library(ggplot2)
 
 # read data
 lifts <- read_excel("workout22.xlsx", sheet = "lifts")
-work <- read_excel("workout22.xlsx", sheet = "workouts")
+work <- read_excel("workout22.xlsx", sheet = "workouts") %>% 
+  mutate(Start_Time = substr(Start_Time, 12, 16)) %>% 
+  mutate(temp_time = round(as.numeric(substr(Start_Time, 4,5))/60,2) * 10000) %>% 
+  mutate(time = as.numeric(paste(substr(Start_Time, 1,2), ".", temp_time, sep = "")))
 
 # calculate max values for each lift for every day I do them
 lift_temp <- lifts %>% 
@@ -39,11 +42,19 @@ ui <- fluidPage(theme = shinytheme("cerulean"),
                   tabPanel("Lift Count", # sidebarPanel
                            mainPanel(
                              h1("Workout Days"),
-                             
-                             h4("Number of days I made it (and didn't make it) to the gym"),
-                             plotOutput("liftcountplot")
+                             fluidRow(splitLayout(cellWidths = c("50%", "50%"),
+                                                  plotOutput("liftcountplot"),
+                                                  plotOutput("locationplot"))
+                             )
                            ) ),
-                  tabPanel("Navbar 3", "This panel is intentionally left blank")
+                  tabPanel("Intensity Tracker",
+                           mainPanel(
+                             h1("Intensity metrics of workouts"),
+                             fluidRow(splitLayout(cellWidths = c("50%", "50%"),
+                                                  plotOutput("caloriesplot"),
+                                                  plotOutput("durationplot"))
+                               )
+                           ))
                   
                 ) # navbarPage
 ) # fluidPage
@@ -69,11 +80,34 @@ server <- function(input, output) {
   
   output$liftcountplot <- renderPlot({
     work %>% 
-      ggplot(., aes(x = Workout)) +
-      geom_bar()
+      ggplot(., aes(x = Workout, fill = Workout)) +
+      geom_bar() +
+      ylab("Total Days")
     
+  })
+  
+  output$caloriesplot <- renderPlot({
+    work %>% 
+      ggplot(., aes(x = Date, y = Calories)) + 
+      geom_line(color = "red") 
+    
+  })
+  
+  output$durationplot <- renderPlot({
+    work %>% 
+      ggplot(., aes(x = Date, y = Duration)) + 
+      geom_line(color = "blue")
+    
+  })
+  
+  output$locationplot <- renderPlot({
+    work %>% 
+      ggplot(., aes(x = Location, fill = Location)) +
+      geom_bar()
   })
 }
 
 # Create Shiny object
 shinyApp(ui = ui, server = server)
+
+# rsconnect::deployApp('~/Downloads/shiny_workout_track')
